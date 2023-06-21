ded28354000fb849bf7b313fd4a386990c03e3d1
Pradhuman Pandey <pradhumanpandeycpp@gmail.com>
2023-06-21 19:35:36 +0530

    Add account services.

---
+++ reception_register_api/src/services/accounts.js
@@ -0,0 +1,32 @@
+import { compareSync } from "bcryptjs";
+
+import { User } from "../models";
+import { generateKey } from "../utils/token";
+
+export async function performLoginService(payload) {
+  const user = await User.findOne({ email: payload.email, isActive: true });
+  if (!user) return null;
+  if (!compareSync(payload.password, user.password)) return null;
+  if (!user.token) {
+    user.token = { key: generateKey() };
+    user.lastLogin = user.token.created;
+    await user.save();
+  }
+  return { token: user.token.key };
+}
+
+export async function retrieveUserService(user) {
+  const detail = {
+    email: user.email,
+    firstName: user.firstName,
+    lastName: user.lastName,
+    dateJoined: user.dateJoined,
+  };
+  return detail;
+}
+
+export async function performLogoutService(user) {
+  user.set("token", undefined, { strict: false });
+  await user.save();
+  return {};
+}
