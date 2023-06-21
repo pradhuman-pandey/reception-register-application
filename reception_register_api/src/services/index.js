603dc53010d1173af404d52e3a29fb37963ef315
Pradhuman Pandey <pradhumanpandeycpp@gmail.com>
2023-06-21 19:35:54 +0530

    Export register services.

---
+++ reception_register_api/src/services/index.js
@@ -0,0 +1,25 @@
+import {
+  performLoginService,
+  performLogoutService,
+  retrieveUserService,
+} from "./accounts";
+import {
+  createRegisterService,
+  destoryRegisterService,
+  listRegisterService,
+  partialUpdateRegisterService,
+  retrieveRegisterService,
+  updateRegisterService,
+} from "./register";
+
+export {
+  createRegisterService,
+  destoryRegisterService,
+  listRegisterService,
+  partialUpdateRegisterService,
+  performLoginService,
+  performLogoutService,
+  retrieveUserService,
+  retrieveRegisterService,
+  updateRegisterService,
+};
