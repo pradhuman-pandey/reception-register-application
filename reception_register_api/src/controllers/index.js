cca8505a73a78211014176f7ee8f0615656c5393
Pradhuman Pandey <pradhumanpandeycpp@gmail.com>
2023-06-21 19:36:29 +0530

    Export all controllers.

---
+++ reception_register_api/src/controllers/index.js
@@ -0,0 +1,21 @@
+import { detail, login, logout } from "./accounts";
+import {
+  createRegister,
+  destoryRegister,
+  listRegister,
+  partialUpdateRegister,
+  retrieveRegister,
+  updateRegister,
+} from "./register";
+
+export {
+  createRegister,
+  destoryRegister,
+  detail,
+  listRegister,
+  login,
+  logout,
+  partialUpdateRegister,
+  retrieveRegister,
+  updateRegister,
+};
