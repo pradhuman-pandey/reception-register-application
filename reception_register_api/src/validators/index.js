0d4b2dd32562ff1193d0f32556696edb58a1246d
Pradhuman Pandey <pradhumanpandeycpp@gmail.com>
2023-06-21 19:34:09 +0530

    Export all validation schemas.

---
+++ reception_register_api/src/validators/index.js
@@ -0,0 +1,4 @@
+import { loginSchema } from './accounts';
+import { registerCreateSchema, registerUpdateSchema } from './register';
+
+export { loginSchema, registerCreateSchema, registerUpdateSchema };
