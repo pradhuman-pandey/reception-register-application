a8d2530cce74cd9b1e0425e837daa49b734ef50e
Pradhuman Pandey <pradhumanpandeycpp@gmail.com>
2023-06-21 20:22:58 +0530

    Fix the placement of ).

--- reception_register_api/src/app.js
+++ reception_register_api/src/app.js
@@ -9,7 +9,7 @@ yargs
       .positional('port', {type: 'number', describe: 'Port', default: 8000})
       .positional('host', {type: 'string', describe: 'Host', default: '127.0.0.1'})
   }, async (args) => {
-    await bootstrap(Number(args.port, String(args.host)));
+    await bootstrap(Number(args.port), String(args.host));
   })
   .command('createsuperuser [email] [firstName] [lastName] [password]', 'Create admin user', (setup) => {
     setup
