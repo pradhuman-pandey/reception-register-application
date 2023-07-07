b02abd2ab83cac466295736b1ff51757d49ef79d
Amogh Madan <amoghmadaan@gmail.com>
2023-07-07 18:28:05 +0530

    Make authentication middleware more robust.

--- reception_register_api/src/middlewares/authentication.js
+++ reception_register_api/src/middlewares/authentication.js
@@ -3,8 +3,9 @@ import {STATUS_CODES} from 'http';
 import {User} from '../models';
 
 const EXCLUDE_URLS = ['/api/v1/accounts/login'];
+
 /**
- *
+ * Authentication middleware.
  * @param {Request} request
  * @param {Response} response
  * @param {NextFunction} next
@@ -12,24 +13,25 @@ const EXCLUDE_URLS = ['/api/v1/accounts/login'];
  */
 export default async function authenticate(request, response, next) {
   const authorization = request.headers?.authorization;
-  if (!EXCLUDE_URLS.includes(request.path)) {
-    if (!authorization) {
-      return response.status(401).json({detail: STATUS_CODES[401]});
-    }
-    const values = authorization.split(' ');
-    if (values.length !== 2) {
-      return response.status(401).json({detail: STATUS_CODES[401]});
-    }
-    if ('Token'.toLowerCase() !== values[0].toLowerCase()) {
-      return response.status(401).json({detail: STATUS_CODES[401]});
-    }
-    const user = await User.findOne({
-      'token.key': values[1],
-    });
-    if (!user) {
-      return response.status(403).json({detail: STATUS_CODES[403]});
-    }
-    request.user = user;
+  if (EXCLUDE_URLS.includes(request.path)) {
+    return next();
   }
-  next();
+  if (!authorization) {
+    return response.status(401).json({detail: STATUS_CODES[401]});
+  }
+  const values = authorization.split(' ');
+  if (values.length !== 2) {
+    return response.status(401).json({detail: STATUS_CODES[401]});
+  }
+  if ('Token'.toLowerCase() !== values[0].toLowerCase()) {
+    return response.status(401).json({detail: STATUS_CODES[401]});
+  }
+  const user = await User.findOne({
+    'token.key': values[1],
+  });
+  if (!user) {
+    return response.status(403).json({detail: STATUS_CODES[403]});
+  }
+  request.user = user;
+  return next();
 }
